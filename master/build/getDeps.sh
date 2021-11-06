37599fb4022d52cb5b6a9bc0a8de5fbb4b73f597
epifanio <epiesasha@me.com>
2021-11-06 14:47:16 +0100

    Update getDeps.sh

--- master/build/getDeps.sh
+++ master/build/getDeps.sh
@@ -6,13 +6,12 @@ apt-get -y update
 # #-------------Application Specific Stuff ----------------------------------------------------
 # removed: doxygen cmake-curses-gui
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get update &&  \
-    apt-get install -y git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
-    libexpat1-dev libfcgi-dev libgsl0-dev libpq-dev libqca2-dev libqca2-plugin-ossl \
-    libqscintilla2-dev libqt4-dev libqt4-opengl-dev libqt4-sql-sqlite \
-    libqtwebkit-dev libqwt-dev libspatialindex-dev libspatialite-dev libsqlite3-dev \
-    lighttpd locales pkg-config poppler-utils pyqt4-dev-tools python python-dev \
-    python-qt4 python-qt4-dev python-qt4-sql python-sip python-sip-dev \
+    apt-get install -y git cmake flex bison libproj-dev libgeos-dev libgdal-dev \
+    libexpat1-dev libfcgi-dev libgs-dev libpq-dev libqca-qt5-2-dev libqca-qt5-2-plugins \
+    libqscintilla2-qt5-dev libqt4-dev libqt5*-dev libsqlite3-dev \
+    lighttpd locales pkg-config poppler-utils pyqt5-dev-tools python python-dev \
+    python3-pyqt5* \
     spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb 
 
-chmod -R a+w /usr/lib/x86_64-linux-gnu/qt4/plugins/designer/
-chmod -R a+w /usr/lib/python2.7/dist-packages/PyQt4/uic/widget-plugins/
+chmod -R a+w /usr/lib/x86_64-linux-gnu/qt5/plugins/designer/
+chmod -R a+w /usr/lib/python3.8/dist-packages/PyQt5/uic/widget-plugins/
